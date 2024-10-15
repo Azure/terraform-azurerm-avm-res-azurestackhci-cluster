462c6818e18eb2a806653edada054c1235c58d76
Hangyu Xu <hangxu@microsoft.com>
2024-10-15 16:50:35 +0800

    use provider::azurerm::normalise_resource_id

--- rolebindings.tf
+++ rolebindings.tf
@@ -21,7 +21,7 @@ resource "azurerm_role_assignment" "machine_role_assign" {
   }
 
   principal_id         = each.value.principal_id
-  scope                = local.key_vault.id
+  scope                = provider::azurerm::normalise_resource_id(local.key_vault.id)
   role_definition_name = each.value.role_name
 
   depends_on = [
